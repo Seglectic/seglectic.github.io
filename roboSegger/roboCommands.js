08791787a385991ff91e2debf62d5709a41a1d28
Segger <saintdane@gmail.com>
2019-06-05 17:41:31 -0400

    Added table
    Added a table for the command list

--- roboSegger/roboCommands.js
+++ roboSegger/roboCommands.js
@@ -1,7 +1,14 @@
-Use this to import roboseg command list and sections
+/*Use this to import roboseg command list and sections
 
 
 Needs to create command object with 3 attributes:
 Category
 Name
 Description
+*/
+
+
+const commands = [
+
+
+]
